dab7e92565c9425e18fb8e653c2023b0a367cef6
unknown <barbenyagre.dmytro@gmail.com>
2023-04-01 14:45:13 +0300

    Full adaptive header

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
-let sliderImg = document.querySelectorAll('.card2');
-let arrowLeft = document.querySelector('#left');
-let arrowRight = document.querySelector('#right');
-let current = 0;
+let sliderImg = document.querySelectorAll('.card2')
+let arrowLeft = document.querySelector('#left')
+let arrowRight = document.querySelector('#right')
+let current = 0
 
 function startSlide() {
-    sliderImg[0].classList.add('active');
+    sliderImg[0].classList.add('active')
 }
 
 function pointSwap() {
@@ -12,38 +12,38 @@ function pointSwap() {
     const activePointIds = ['1', '2', '3', '4'];
 
     for (let i = 0; i < activeCardIds.length; i++) {
-        const activeCardId = activeCardIds[i];
-        const activePointId = activePointIds[i];
+        const activeCardId = activeCardIds[i]
+        const activePointId = activePointIds[i]
 
         if (document.getElementById(activeCardId).classList.contains('active')) {
-            const points = document.querySelectorAll('.point');
-            points.forEach(point => point.classList.remove('point-active'));
-            document.getElementById(activePointId).classList.add('point-active');
-            break;
+            const points = document.querySelectorAll('.point')
+            points.forEach(point => point.classList.remove('point-active'))
+            document.getElementById(activePointId).classList.add('point-active')
+            break
         }
     }
 }
 
 function slideLeft() {
-    sliderImg[current].classList.remove('active');
-    current--;
+    sliderImg[current].classList.remove('active')
+    current--
     if (current < 0) {
-        current = sliderImg.length - 1;
+        current = sliderImg.length - 1
     }
-    sliderImg[current].classList.add('active');
-    pointSwap();
+    sliderImg[current].classList.add('active')
+    pointSwap()
 }
 
 arrowLeft.addEventListener('click', slideLeft);
 
 function slideRight() {
-    sliderImg[current].classList.remove('active');
-    current++;
+    sliderImg[current].classList.remove('active')
+    current++
     if (current === sliderImg.length) {
-        current = 0;
+        current = 0
     }
-    sliderImg[current].classList.add('active');
-    pointSwap();
+    sliderImg[current].classList.add('active')
+    pointSwap()
 }
 
 arrowRight.addEventListener('click', slideRight);
@@ -54,10 +54,10 @@ const points = document.querySelectorAll('.point');
 
 points.forEach(point => {
     point.addEventListener('click', () => {
-        sliderImg.forEach(slide => slide.classList.remove('active'));
-        const cardId = point.id.replace('point', '') + 'card';
-        document.getElementById(cardId).classList.add('active');
-        current = parseInt(point.id) - 1;
-        pointSwap();
-    });
-});
+        sliderImg.forEach(slide => slide.classList.remove('active'))
+        const cardId = point.id.replace('point', '') + 'card'
+        document.getElementById(cardId).classList.add('active')
+        current = parseInt(point.id) - 1
+        pointSwap()
+    })
+})
